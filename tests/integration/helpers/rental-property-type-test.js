9d1c5bdcd239f107e571d50cd97cda179ce79c71
Rafael Ferraro <ferraromv@gmail.com>
2017-11-08 19:23:36 -0200

    Refactoring spec

--- tests/integration/helpers/rental-property-type-test.js
+++ tests/integration/helpers/rental-property-type-test.js
@@ -12,16 +12,12 @@ test('it renders Standalone when property is not part of a community', function(
 });
 
 test('it renders Community when property is part of a community', function(assert) {
-    this.set('inputValue', 'Apartment');
-    this.render(hbs`{{rental-property-type inputValue}}`);
-    assert.equal(this.$().text().trim(), 'Community');
-
-    this.set('inputValue', 'Townhouse');
-    this.render(hbs`{{rental-property-type inputValue}}`);
-    assert.equal(this.$().text().trim(), 'Community');
-
-    this.set('inputValue', 'Condo');
-    this.render(hbs`{{rental-property-type inputValue}}`);
-    assert.equal(this.$().text().trim(), 'Community');
+  const self = this;
+  const properties = ['Apartment', 'Townhouse', 'Condo'];
+  properties.forEach(function (value) {
+    self.set('inputValue', 'Apartment');
+    self.render(hbs`{{rental-property-type inputValue}}`);
+    assert.equal(self.$().text().trim(), 'Community');
+  });
 });
 
